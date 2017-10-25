1d4d5acf2e51a8bfc8b1ecc55dc3149a9284e21f
Marty McLaughlin <martymclaugh@gmail.com>
2017-10-24 18:41:43 -0700

    add logic to update cargo

--- src/components/HeadQuarters/HeadQuarters.js
+++ src/components/HeadQuarters/HeadQuarters.js
@@ -1,6 +1,7 @@
 // @flow
 
 import React, { Component } from 'react';
+import { fromJS } from 'immutable';
 import { connect } from 'react-redux';
 import Terminal from '../Terminal/Terminal';
 import randomStringArray from '../../helpers/random-string';
@@ -24,7 +25,7 @@ type Props = {
   terminalAmount: number,
   terminals: Array<TerminalType>,
   setTerminals: (Object) => void,
-  updateStorage: () => void,
+  updateCargo: () => void,
   setHackNumber: ({ numberOfHacks: number }) => void,
 }
 
@@ -39,12 +40,14 @@ class HeadQuarters extends Component<Props, State>{
     this.state = {
       hackingActive: false,
       emptyTerminals: 0,
+      emptyTerminalIds: [],
     }
 
     this.initiateHack = this.initiateHack.bind(this);
     this.handleHacking = this.handleHacking.bind(this);
     this.handleDiscardTerminal = this.handleDiscardTerminal.bind(this);
     this.populateTerminals = this.populateTerminals.bind(this);
+    this.terminateHacking = this.terminateHacking.bind(this);
   }
 
   componentDidMount() {
@@ -66,34 +69,30 @@ class HeadQuarters extends Component<Props, State>{
 
       return terminal;
     });
-
     this.props.setTerminals({ terminals });
     this.props.setHackNumber({ numberOfHacks: this.props.numberOfHacks - 1 });
   }
-
   handleHacking: () => void;
   handleHacking() {
     if (this.props.numberOfHacks > 0 && this.state.emptyTerminals > 0) {
       this.initiateHack();
-    } else {
-      this.props.updateStorage();
+      this.setState({ emptyTerminals: 0 });
     }
   }
-
   handleDiscardTerminal: (TerminalType) => void;
   handleDiscardTerminal(terminal) {
     if (this.props.numberOfHacks > 0) {
       const terminals = this.props.terminals.update(this.props.terminals.indexOf(terminal), term => {
-          return { id: term.id, value: null};
+          this.setState({
+            emptyTerminals: this.state.emptyTerminals + 1,
+            emptyTerminalIds: this.state.emptyTerminalIds.concat(term.get('id')),
+          });
+          return fromJS({ id: term.get('id'), value: null});
       });
 
       this.props.setTerminals({ terminals });
-      this.setState({
-         emptyTerminals: this.state.emptyTerminals + 1,
-      });
     }
   }
-
   populateTerminals: () => void;
   populateTerminals() {
     const terminals = []
@@ -108,14 +107,28 @@ class HeadQuarters extends Component<Props, State>{
       emptyTerminals: this.props.terminalAmount,
     });
   }
-
+  terminateHacking() {
+    this.setState({ emptyTerminalIds: [] });
+    if (this.props.numberOfHacks === 0) {
+      // on the last hack
+      this.props.updateCargo();
+    }
+  }
   render() {
-    const renderTerminals = this.props.terminals.map((terminal, i) => (
+    const {
+      terminals,
+      numberOfHacks,
+      terminalAmount,
+    } = this.props;
+
+    const renderTerminals = terminals.map((terminal, i) => (
       <Terminal
         key={i}
-        numberOfHacks={this.props.numberOfHacks}
+        numberOfHacks={numberOfHacks}
         hackingActive={this.state.hackingActive}
         onClick={() => this.handleDiscardTerminal(terminal)}
+        isLastTerminal={Math.max(...this.state.emptyTerminalIds) === terminal.get('id')}
+        terminateHacking={this.terminateHacking}
         algorithm={terminal.value && [...randomStringArray(12 + (i * 5)), terminal.value.name]}
         {...terminal}
       />
@@ -124,7 +137,7 @@ class HeadQuarters extends Component<Props, State>{
     return (
       <div>
         <div>Head Quarters</div>
-        <div>Number of Hacks: {this.props.numberOfHacks}</div>
+        <div>Number of Hacks: {numberOfHacks}</div>
         <button onClick={this.handleHacking}>Generate Resources</button>
         <div className="head-quarters__terminals">
           {renderTerminals}
