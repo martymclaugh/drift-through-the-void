2512f43546feec8ab4854deeac36c20720bdf374
Marty McLaughlin <martymclaugh@gmail.com>
2017-10-24 18:42:57 -0700

    add resource display component, display cargo

--- src/components/ResourceDisplay/ResourceDisplay.js
+++ src/components/ResourceDisplay/ResourceDisplay.js
@@ -1,23 +1,49 @@
 import React, { Component } from 'react';
+import { toJS } from 'immutable';
 import { connect } from 'react-redux';
+import CargoContainer from '../shared/CargoContainer/CargoContainer';
+
+import './resource-display-styles.css';
 
 class ResourceDisplay extends Component {
   constructor(props) {
     super(props);
 
     this.state = {}
+
+    this.renderCargoContainers = this.renderCargoContainers.bind(this);
+  }
+
+  renderCargoContainers() {
+    const distributedResources = this.props.distributedResources.toJS();
+    let containers = [];
+
+    for (var key in distributedResources){
+      if (distributedResources.hasOwnProperty(key)){
+        containers.push(
+          <CargoContainer
+            amount={distributedResources[key]}
+            name={key}
+          />
+        )
+      }
+    }
+
+    return containers.reverse();
   }
 
   render() {
-    console.log(this.props.storage);
     return (
-      <div>ResourceDisplay</div>
+      <div className="resource-display">
+        Cargo Hold
+        {this.renderCargoContainers()}
+      </div>
     )
   }
 }
 
 const mapStateToProps = state => ({
-  storage: state.playerBoard
+  distributedResources: state.playerBoard.get('distributedResources'),
 });
 
 export default connect(mapStateToProps, {  })(ResourceDisplay);
