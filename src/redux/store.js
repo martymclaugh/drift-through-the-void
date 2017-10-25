5d11c554ea367a6349755e095b3722fd2c6ed1ec
Marty McLaughlin <martymclaugh@gmail.com>
2017-10-24 18:44:07 -0700

    add redux logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,34 @@
 import { createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
+import { Iterable } from 'immutable';
+import { createLogger } from 'redux-logger';
 import rootReducer from './reducers';
 // import rootSaga from './sagas';
 // import { routerMiddleware } from 'react-router-redux'
 // import createHistory from 'history/createBrowserHistory'
-const middlewares = [
-  logger,
-]
+
+const middlewares = [];
+// move into separate file if add more middlewares
+if (process.env.NODE_ENV === `development`) {
+  const stateTransformer = (state) => {
+    const newState = {};
+    Object.keys(state).forEach((i) => {
+      if (Iterable.isIterable(state[i])) {
+        newState[i] = state[i].toJS();
+      } else {
+        newState[i] = state[i];
+      }
+    });
+
+    return newState;
+  };
+
+  const loggerMiddleware = createLogger({
+    stateTransformer,
+  });
+
+  middlewares.push(loggerMiddleware);
+}
+
 const store = createStore(
   rootReducer,
   applyMiddleware(...middlewares),
