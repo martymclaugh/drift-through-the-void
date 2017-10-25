61023ff314f2c65b790112dd411d3a5d206bdd03
Marty McLaughlin <martymclaugh@gmail.com>
2017-10-24 18:43:45 -0700

    add helper to easily map resources

---
+++ src/helpers/resource-map.js
@@ -0,0 +1,48 @@
+import nanoTubesGreen from '../assets/images/resource-images/nano-green.png';
+import nanoTubesBlack from '../assets/images/resource-images/nano-black.png';
+
+import unobtaniumGreen from '../assets/images/resource-images/unobtanium-green.png';
+import unobtaniumBlack from '../assets/images/resource-images/unobtanium-black.png';
+
+import planktonGreen from '../assets/images/resource-images/plankton-green.png';
+import planktonBlack from '../assets/images/resource-images/plankton-black.png';
+
+import spiderSilkGreen from '../assets/images/resource-images/spider-green.png';
+import spiderSilkBlack from '../assets/images/resource-images/spider-black.png';
+
+import riflesGreen from '../assets/images/resource-images/rifle-green.png';
+import riflesBlack from '../assets/images/resource-images/rifle-black.png';
+
+
+export const resourceMap = {
+  nanoTubes: {
+    firstVal: 1,
+    maxAmount: 8,
+    greenImg: nanoTubesGreen,
+    blackImg: nanoTubesBlack,
+  },
+  unobtanium: {
+    firstVal: 2,
+    maxAmount: 7,
+    greenImg: unobtaniumGreen,
+    blackImg: unobtaniumBlack,
+  },
+  energyPlankton: {
+    firstVal: 3,
+    maxAmount: 6,
+    greenImg: planktonGreen,
+    blackImg: planktonBlack,
+  },
+  giantSpiderSilk: {
+    firstVal: 4,
+    maxAmount: 5,
+    greenImg: spiderSilkGreen,
+    blackImg: spiderSilkBlack,
+  },
+  rifles: {
+    firstVal: 5,
+    maxAmount: 4,
+    greenImg: riflesGreen,
+    blackImg: riflesBlack,
+  },
+}
