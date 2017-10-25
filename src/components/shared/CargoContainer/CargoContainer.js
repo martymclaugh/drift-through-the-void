bd2d5b96d6c7e6b07c7c5aa50eac962847b885b0
Marty McLaughlin <martymclaugh@gmail.com>
2017-10-24 18:43:22 -0700

    add cargo container

---
+++ src/components/shared/CargoContainer/CargoContainer.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { resourceMap } from '../../../helpers/resource-map';
+
+import './cargo-container-styles.css';
+
+const renderIcons = (props) => {
+  const resource = resourceMap[props.name];
+  const maxAmount = resource.maxAmount;
+  let icons = [];
+
+  for(var i = 1; i < maxAmount + 1; i++) {
+    const isActive = i < props.amount + 1;
+    const imgSource = isActive ? resource.greenImg : resource.blackImg;
+
+    icons.push(
+      <div className="icon">
+        <div className="icon__value">{i * resource.firstVal}</div>
+        <div className="icon__asset">
+          <img className={`icon__asset-image ${isActive ? 'is-active' : ''}`} src={imgSource} alt=""/>
+        </div>
+      </div>
+    )
+  }
+
+  return icons;
+}
+export default (props) => {
+  const totalValue = resourceMap[props.name].firstVal * props.amount;
+  return (
+    <div>{totalValue}{renderIcons(props)}</div>
+  )
+}
