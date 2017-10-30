28cc81d976303f7fe9d772625914f0eb74fef685
Marty McLaughlin <martymclaugh@gmail.com>
2017-10-29 17:17:16 -0700

    add gameplay middleware and separate middlewares for organization

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 import App from './App';
 
-
 render(
   <Provider store={store}>
     <App />
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,38 +1,17 @@
 import { createStore, applyMiddleware } from 'redux';
-import { Iterable } from 'immutable';
-import { createLogger } from 'redux-logger';
 import rootReducer from './reducers';
+import middlewares from './middlewares';
+import { startGame } from './game';
 // import rootSaga from './sagas';
 // import { routerMiddleware } from 'react-router-redux'
 // import createHistory from 'history/createBrowserHistory'
-
-const middlewares = [];
-// move into separate file if add more middlewares
-if (process.env.NODE_ENV === `development`) {
-  const stateTransformer = (state) => {
-    const newState = {};
-    Object.keys(state).forEach((i) => {
-      if (Iterable.isIterable(state[i])) {
-        newState[i] = state[i].toJS();
-      } else {
-        newState[i] = state[i];
-      }
-    });
-
-    return newState;
-  };
-
-  const loggerMiddleware = createLogger({
-    stateTransformer,
-  });
-
-  middlewares.push(loggerMiddleware);
-}
 
 const store = createStore(
   rootReducer,
   applyMiddleware(...middlewares),
 );
+
+startGame(store);
 
 // sagaMiddleware.run(rootSaga);
 
