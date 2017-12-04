3969e2c4ca434491b014a912256c7f9d7cc74ce3
Marty McLaughlin <martymclaugh@gmail.com>
2017-12-03 17:02:25 -0800

    add create username and send lobby message actions

--- src/redux/game/game-actions.js
+++ src/redux/game/game-actions.js
@@ -2,6 +2,8 @@ export const types = {
   SEND_HACK_NUMBER: 'SEND_HACK_NUMBER',
   SEND_TERMINALS: 'SEND_TERMINALS',
   SEND_CARGO: 'SEND_CARGO',
+  CREATE_USERNAME: 'CREATE_USERNAME',
+  SEND_LOBBY_MESSAGE: 'SEND_LOBBY_MESSAGE',
 }
 
 export const sendHackNumber = payload =>
