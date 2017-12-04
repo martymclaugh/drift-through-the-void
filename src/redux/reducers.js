3b839fe99682b10c26b72a18db29aef5d5b96db8
Marty McLaughlin <martymclaugh@gmail.com>
2017-12-03 17:04:08 -0800

    add home screen reducer and lobby reducer

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -2,8 +2,12 @@ import { combineReducers } from 'redux';
 // import { routerReducer } from 'react-router-redux';
 import headQuartersReducer from '../components/HeadQuarters/head-quarters-reducer';
 import playerBoardReducer from '../components/PlayerBoard/player-board-reducer';
+import homeScreenReducer from '../components/HomeScreen/home-screen-reducer';
+import lobbyReducer from '../components/LobbyScreen/lobby-screen-reducer';
 
 export default combineReducers({
   headQuarters: headQuartersReducer,
   playerBoard: playerBoardReducer,
+  homeScreen: homeScreenReducer,
+  lobby: lobbyReducer,
 });
