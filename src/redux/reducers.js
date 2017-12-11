1f9417518563d002c6277ddb3b5325a85ba67626
Marty McLaughlin <martymclaugh@gmail.com>
2017-12-11 12:07:47 -0800

    add planet reducer

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -4,10 +4,12 @@ import headQuartersReducer from '../components/HeadQuarters/head-quarters-reduce
 import playerBoardReducer from '../components/PlayerBoard/player-board-reducer';
 import homeScreenReducer from '../components/HomeScreen/home-screen-reducer';
 import lobbyReducer from '../components/LobbyScreen/lobby-screen-reducer';
+import planetReducer from '../components/PlanetContainer/planet-reducer'
 
 export default combineReducers({
   headQuarters: headQuartersReducer,
   playerBoard: playerBoardReducer,
   homeScreen: homeScreenReducer,
   lobby: lobbyReducer,
+  planets: planetReducer,
 });
