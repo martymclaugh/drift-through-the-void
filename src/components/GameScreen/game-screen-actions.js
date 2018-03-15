d22c034c8d6d43f7fbd706c7f9b16574ac597949
Marty McLaughlin <martymclaugh@gmail.com>
2018-03-14 23:25:06 -0700

    add server confirmation actions and reducer

--- src/components/GameScreen/game-screen-actions.js
+++ src/components/GameScreen/game-screen-actions.js
@@ -1,6 +1,9 @@
 export const types = {
   UPDATE_CARGO: 'UPDATE_CARGO',
+  RECEIVE_SERVER_CONFIRMATION: 'RECEIVE_SERVER_CONFIRMATION',
 }
 
 export const updateCargo = payload =>
   ({ type: types.UPDATE_CARGO, payload })
+export const receiveServerConfirmation = payload =>
+  ({ type: types.RECEIVE_SERVER_CONFIRMATION, payload });
--- src/components/GameScreen/game-screen-reducer.js
+++ src/components/GameScreen/game-screen-reducer.js
@@ -3,25 +3,33 @@ import { types } from './game-screen-actions';
 import { planetActionTypes } from '../PlanetContainer/planet-actions';
 
 const INITIAL_STATE = fromJS({
-  colonists: 0,
-  soylent: 0,
-  credits: 0,
-  distributedResources: {
-    nanoTubes: 0,
-    unobtanium: 0,
-    energyPlankton: 0,
-    giantSpiderSilk: 0,
-    rifles: 0,
+  resources: {
+    colonists: 0,
+    soylent: 0,
+    credits: 0,
+    distributedResources: {
+      nanoTubes: 0,
+      unobtanium: 0,
+      energyPlankton: 0,
+      giantSpiderSilk: 0,
+      rifles: 0,
+    },
   },
+  server: '',
+  serverConfirmed: true,
 });
 
 
 const gameScreenReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case types.UPDATE_CARGO:
+    // TODO fix below
       return state.merge(action.payload);
     case planetActionTypes.COLONIZE_PLANET:
-      return state.set('colonists', state.get('colonists') - 1);
+      return state.set('colonists', state.getIn(['resources', 'colonists']) - 1);
+    case types.RECEIVE_SERVER_CONFIRMATION:
+      console.log(action.payload);
+      return state.merge(action.payload);
     default:
       return state;
   }
