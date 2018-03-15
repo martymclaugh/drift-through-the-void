f96bfe6652e1cb2ab432aabee28dfdea09920d26
Marty McLaughlin <marty@fandor.com>
2018-03-15 11:01:53 -0700

    remove unused actions

--- src/components/GameScreen/game-screen-actions.js
+++ src/components/GameScreen/game-screen-actions.js
@@ -1,9 +1,6 @@
 export const types = {
   UPDATE_CARGO: 'UPDATE_CARGO',
-  RECEIVE_SERVER_CONFIRMATION: 'RECEIVE_SERVER_CONFIRMATION',
 }
 
 export const updateCargo = payload =>
-  ({ type: types.UPDATE_CARGO, payload })
-export const receiveServerConfirmation = payload =>
-  ({ type: types.RECEIVE_SERVER_CONFIRMATION, payload });
+  ({ type: types.UPDATE_CARGO, payload });
