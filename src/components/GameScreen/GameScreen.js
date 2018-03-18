4864ff5c87d97cd0cb899b2e4493896077f07869
Marty McLaughlin <marty@fandor.com>
2018-03-18 12:01:45 -0700

    modify to render phase screens

--- src/components/GameScreen/GameScreen.js
+++ src/components/GameScreen/GameScreen.js
@@ -2,90 +2,69 @@
 
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { updateCargo, startGame } from './game-screen-actions';
-import { sendCargo } from '../../redux/socket/game-actions';
-import { reduceObjectValues } from '../../helpers/reduce-object-value';
-import HeadQuarters from '../HeadQuarters/HeadQuarters';
-import ResourceDisplay from '../ResourceDisplay/ResourceDisplay';
-import PlanetContainer from '../PlanetContainer/PlanetContainer';
-import MonumentsContainer from '../MonumentsContainer/MonumentsContainer';
-import { Props, State } from '../../flow/components/game-screen-types';
+import { startGame, changePhase } from './game-screen-actions';
+import { gamePhases } from '../../helpers/game-phases';
+import GenerateResources from '../GenerateResources/GenerateResources';
+import PlanetsContainer from '../PlanetsContainer/PlanetsContainer';
 
 import './game-screen-styles.css';
 
-class GameScreen extends Component<Props, State>{
+class GameScreen extends Component {
 
-  constructor(props: Props) {
+  constructor(props) {
     super(props)
 
-    this.state = {
-      activePlayer: '',
-    }
+    this.state = {}
 
-    this.updateCargo = this.updateCargo.bind(this);
+    this.renderPhaseScreen = this.renderPhaseScreen.bind(this);
   }
-
   componentWillMount() {
     const server = this.props.match.params.slug;
+
     if (!this.props.games.get(server)) {
       this.props.history.push(`/lobby`);
     }
   }
   componentWillReceiveProps(nextProps) {
-    console.log('NEXT PROPS', nextProps);
+    // TODO game starting conditions
     // if (nextProps.playersJoined === nextProps.numberOfPlayers && !nextProps.gameStarted) {
 
-      if (nextProps.users.size > 0){
+      if (nextProps.users.size > 0 && !nextProps.gameStarted){
         this.props.startGame();
       }
     // }
   }
-  updateCargo: () => void;
-  updateCargo() {
-    const { terminals, cargo } = this.props;
-    // const death = reduceObjectValues(terminals, 'death');
-    const resources = reduceObjectValues(terminals, 'resources')
-    let cargoItems = {
-      colonists: cargo.get('colonists'),
-      soylent: cargo.get('soylent'),
-      credits: cargo.get('credits'),
+  renderPhaseScreen: () => void;
+  renderPhaseScreen() {
+    switch (this.props.phase) {
+      case gamePhases.GENERATE_RESOURCES:
+        return (
+          <GenerateResources
+            changePhase={this.props.changePhase}
+          />
+        );
+      case gamePhases.POPULATE_PLANETS:
+        return (
+          <PlanetsContainer
+            changePhase={this.props.changePhase}
+          />
+        );
+      default:
+        return (
+          <GenerateResources
+            changePhase={this.props.changePhase}
+          />
+        );
     }
-    let distributedResources = cargo.get('distributedResources').toJS();
-    const resourceKeys = Object.keys(distributedResources);
-
-    Object.keys(cargoItems).map(key => {
-      cargoItems[key] = cargoItems[key] + reduceObjectValues(terminals, key);
-
-      return cargoItems;
-    });
-    for (var i = 0; i < resources; i ++) {
-      const key = resourceKeys[i % resourceKeys.length];
-      distributedResources[key] = distributedResources[key] + 1;
-    }
-
-    const distributedItems = {
-      ...cargoItems,
-      distributedResources,
-    }
-    const payload = {
-      data: distributedItems,
-      // server:
-    }
-    this.props.updateCargo(distributedItems);
-    this.props.sendCargo(distributedItems);
   }
   render() {
     const playersNeeded = this.props.numberOfPlayers - this.props.playersJoined;
     if (this.props.gameStarted) {
       return (
         <div className='game-screen'>
-          <ResourceDisplay />
-          <HeadQuarters
-            terminalAmount={7}
-            updateCargo={this.updateCargo}
-          />
+          {this.renderPhaseScreen()}
         </div>
-      );
+      )
     }
     return (
       <div>Still waiting for {playersNeeded} players...</div>
@@ -93,23 +72,16 @@ class GameScreen extends Component<Props, State>{
   }
 }
 
-const mapStateToProps = state => {
-  const activePlayer = state.gameScreen.get('activePlayer');
-
-  return ({
-    numberOfHacks: state.headQuarters.get('numberOfHacks'),
-    terminals: state.headQuarters.get('terminals'),
-    numberOfPlayers: state.gameScreen.get('numberOfPlayers'),
-    playersJoined: state.gameScreen.get('playersJoined'),
-    gameStarted: state.gameScreen.get('gameStarted'),
-    users: state.gameScreen.get('users'),
-    cargo: state.gameScreen.getIn(['users', `${activePlayer}`, 'resources']),
-    games: state.lobby.get('games'),
-  });
-}
+const mapStateToProps = state => ({
+  numberOfPlayers: state.gameScreen.get('numberOfPlayers'),
+  playersJoined: state.gameScreen.get('playersJoined'),
+  gameStarted: state.gameScreen.get('gameStarted'),
+  users: state.gameScreen.get('users'),
+  games: state.lobby.get('games'),
+  phase: state.gameScreen.get('phase'),
+});
 
 export default connect(mapStateToProps, {
-  updateCargo,
-  sendCargo,
+  changePhase,
   startGame,
 })(GameScreen);
