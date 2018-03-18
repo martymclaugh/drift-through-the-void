4d055b01f809f3932736c5b53b383dd4595707ca
Marty McLaughlin <marty@fandor.com>
2018-03-18 12:01:22 -0700

    add change phase and colonize planet actions

--- src/components/GameScreen/game-screen-actions.js
+++ src/components/GameScreen/game-screen-actions.js
@@ -2,6 +2,8 @@ export const types = {
   UPDATE_CARGO: 'UPDATE_CARGO',
   PLAYER_JOINED: 'PLAYER_JOINED',
   START_GAME: 'START_GAME',
+  CHANGE_PHASE: 'CHANGE_PHASE',
+  COLONIZE_PLANET: 'COLONIZE_PLANET',
 }
 
 export const updateCargo = payload =>
@@ -10,3 +12,7 @@ export const startGame = payload =>
   ({ type: types.START_GAME, payload });
 export const playerJoined = payload =>
   ({ type: types.PLAYER_JOINED, payload });
+export const changePhase = () =>
+  ({ type: types.CHANGE_PHASE });
+export const colonizePlanet = payload =>
+  ({ type: types.COLONIZE_PLANET, payload });
