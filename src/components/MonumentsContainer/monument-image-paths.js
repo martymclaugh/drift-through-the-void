96ba6abdddf789e4ab95f8edff76ad09c0878215
Marty McLaughlin <marty@fandor.com>
2018-03-20 10:04:33 -0700

    change space portal srC

--- src/components/MonumentsContainer/monument-image-paths.js
+++ src/components/MonumentsContainer/monument-image-paths.js
@@ -3,7 +3,7 @@ import promethius from '../../assets/images/monument-images/promethius.png';
 import spaceChurch from '../../assets/images/monument-images/space-church.png';
 import spaceLadder from '../../assets/images/monument-images/space-ladder.png';
 import sphereOFear from '../../assets/images/monument-images/sphere-o-fear.png';
-import spacePortal from '../../assets/images/monument-images/space-portal.png';
+import spacePortal from '../../assets/images/monument-images/space-portal.svg';
 import tesseract from '../../assets/images/monument-images/tesseract.png';
 
 const imagePaths = {
