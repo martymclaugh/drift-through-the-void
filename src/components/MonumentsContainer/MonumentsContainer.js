c8d0ed3ae3e037da32de45c60bdd3ca443b57a78
Marty McLaughlin <marty@fandor.com>
2018-03-20 10:03:49 -0700

    colonize monument functionality

--- src/components/MonumentsContainer/MonumentsContainer.js
+++ src/components/MonumentsContainer/MonumentsContainer.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { colonizeMonument } from './monument-actions';
+import { colonizeMonument } from '../GameScreen/game-screen-actions';
 import monumentImagePaths from './monument-image-paths';
 import Monument from '../shared/Monument/Monument';
+import Button from '../shared/Button/Button';
 
 import './monuments-container-styles.css';
 
@@ -10,32 +11,71 @@ class MonumentsContainer extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {}
+    this.state = {
+      finishedColonizing: false,
+    }
     this.renderMonuments = this.renderMonuments.bind(this);
+  }
+  handleMonumentClick: () => void;
+  handleMonumentClick(name) {
+    const {
+      monuments,
+      colonists,
+      colonizeMonument,
+      isActivePlayer
+    } = this.props;
+
+    if (monuments.getIn([name, 'requiredColonists']) > 0 && colonists > 0 && isActivePlayer) {
+      colonizeMonument({ name });
+    }
+  }
+  finishColonizing: () => void;
+  finishColonizing() {
+    this.setState({ finishedColonizing: true });
+
+    // setTimeout(() => {
+      this.props.changePhase();
+    // }, 2000)
   }
   renderMonuments: () => void;
   renderMonuments() {
-    return this.props.monuments.map(monument => (
-      <Monument
-        imagePath={monumentImagePaths[monument.get('id')].imagePath}
-        requiredColonists={monument.get('requiredColonists')}
-        pointsIfFirst={monument.get('pointsIfFirst')}
-        pointsIfNotFirst={monument.get('pointsIfNotFirst')}
-      />
-    ));
+    const monumentKeys = this.props.monuments.keySeq().toArray()
+    return monumentKeys.map(key => {
+      const monument = this.props.monuments.get(key);
+      return (
+        <Monument
+          class={key}
+          canColonize={this.props.colonists > 0}
+          isActivePlayer={this.props.isActivePlayer}
+          onClick={() => this.handleMonumentClick(key)}
+          imagePath={monumentImagePaths[key].imagePath}
+          requiredColonists={monument.get('requiredColonists')}
+          pointsIfFirst={monument.get('pointsIfFirst')}
+          pointsIfNotFirst={monument.get('pointsIfNotFirst')}
+        />
+      )
+    });
   }
   render() {
+    const { isActivePlayer } = this.props;
+    const button = isActivePlayer && !this.state.finishedColonizing && (
+      <Button onClick={() => this.finishColonizing()}>
+        Finish Colonizing
+      </Button>
+    )
     return (
       <div className="monuments-container">
         {this.renderMonuments()}
+        {button}
       </div>
     )
   }
 }
 
 const mapStateToProps = state => ({
-  monuments: state.monuments,
-  colonists: state.gameScreen.get('colonists'),
+  monuments: state.gameScreen.getIn(['users', `${state.gameScreen.get('activePlayer')}`, 'monuments']),
+  colonists: state.gameScreen.getIn(['users', `${state.gameScreen.get('activePlayer')}`, 'resources', 'colonists']),
+  isActivePlayer: state.homeScreen.get('username') === state.gameScreen.get('activePlayer'),
 });
 
 export default connect(mapStateToProps, { colonizeMonument })(MonumentsContainer);
