dc8625cbcd4e8ef6d834c94c3872372ce9c227ca
Marty McLaughlin <marty@fandor.com>
2018-04-01 12:56:29 -0700

    add classnames and logic for trading resources

--- src/components/ResourceDisplay/CargoContainer/CargoContainer.js
+++ src/components/ResourceDisplay/CargoContainer/CargoContainer.js
@@ -2,23 +2,42 @@
 
 import React from 'react';
 import { resourceMap } from '../../../helpers/resource-map';
+import { gamePhases } from '../../../helpers/game-phases';
 import { Props } from '../../../flow/components/cargo-container-types';
 
 import './cargo-container-styles.css';
 
 const resourceBar = (props: Props) => {
-  const resource = resourceMap[props.name];
+  const {
+    name,
+    phase,
+    engine,
+    amount,
+    isActivePlayer
+  } = props;
+  const resource = resourceMap[name];
   const maxAmount = resource.maxAmount;
   let resourcePoints = [];
   const imgSource = resource.greenImg;
   const pointBoxWidth = 100 / maxAmount;
+  const canTrade = name ===  'unobtanium' &&
+                   engine &&
+                   phase === gamePhases.TRADE_RESOURCES;
 
-  for (let i = 1; i < maxAmount + 1; i++) {
-    const isActive = i < props.amount + 1;
+  for (let i = 0; i < maxAmount; i++) {
+    const isActive = i < amount;
+    const isSelected = i >= props.selectedResources.get('unobtanium') - 1;
+    const classNames = `resource__points-box ${isActive ? 'is-active' : ''}` +
+                       ` ${canTrade && isActive ? 'is-tradable' : ''}` +
+                       ` ${isSelected ? 'is-selected' : ''}`;
 
     resourcePoints.push(
-      <div className={`resource__points-box ${isActive ? 'is-active' : ''}`}>
-        <div className="resource__value">{i * resource.increment}</div>
+      <div
+        // we have to add 1 to the index since 0 is falsy for the check in upgrades reducer
+        onClick={canTrade && isActive ? () => props.handleSelectResource({ [name]: i + 1 }) : null}
+        className={classNames}
+      >
+        <div className="resource__value">{(i + 1) * resource.increment}</div>
       </div>
     );
   }
